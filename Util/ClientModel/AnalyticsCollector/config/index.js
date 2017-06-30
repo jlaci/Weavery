9df4a1d3970d84d4d205454e212599b4e5fcbcb7
Jánoky Laszló Viktor <janoky.laszlo@bmeautsoft.hu>
2017-06-30 15:30:56 +0200

    ClientProbe progress

--- Util/ClientModel/AnalyticsCollector/config/index.js
+++ Util/ClientModel/AnalyticsCollector/config/index.js
@@ -2,6 +2,6 @@ var config = {};
 config.port = process.env.PORT || 8888;
 config.mongoUri = process.env.MONGODB_URI || 'mongodb://analytics:analpw@ds139122.mlab.com:39122/participatory';
 
-config.https = 'self-signed'; //Other possiblities are: 'no', 'self-signed', 'valid-cert', 'lets-encrypt'
+config.https = process.env.HTTPS_MODE || 'no'; //Other possiblities are: 'no', 'self-signed', 'valid-cert', 'lets-encrypt'
 
 module.exports = config;
